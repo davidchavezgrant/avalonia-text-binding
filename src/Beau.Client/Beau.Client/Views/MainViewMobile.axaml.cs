f05ac2dd3388c185e6cfb745368804f87c20dcd8
davidchavezgrant <david@lines.by>
2023-05-03 04:22:39 -0400

    Mobile and desktop views

---
+++ src/Beau.Client/Beau.Client/Views/MainViewMobile.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+
+namespace Beau.Client.Views;
+
+public partial class MainViewMobile: UserControl
+{
+    public MainViewMobile()
+    {
+        InitializeComponent();
+    }
+
+}
