f74253afa5d340b37e889d65474f778de90487b8
davidchavezgrant <david@lines.by>
2023-05-10 01:19:25 -0400

    box

--- src/Beau.Client/Beau.Client/Views/MainViewMobile.axaml.cs
+++ src/Beau.Client/Beau.Client/Views/MainViewMobile.axaml.cs
@@ -16,32 +16,27 @@ public partial class MainViewMobile: UserControl
     public MainViewMobile()
     {
         InitializeComponent();
-        this.CreateAccountButton.Click +=(_,_) => this.Click();
         StartAnimations();
     }
 
-    void Click()
+    void CreateAccount()
     {
-        HapticFeedback.Perform(HapticFeedbackType.Click);
-        var contact = new CNMutableContact();
-        contact.GivenName = "John";
-        contact.FamilyName = "Appleseed";
-
-        var store = new CNContactStore();
-        var saveRequest = new CNSaveRequest();
-        saveRequest.AddContact(contact, null);
-        store.ExecuteSaveRequest(saveRequest, out var error);
 
     }
+
+    void SignIn()
+    {
+
+    }
+
 
     private async void StartAnimations()
     {
         var logo         = this.FindControl<Image>(nameof(this.Logo));
-        var signIn       = this.FindControl<Border>(nameof(this.SignIn));
-        var signInButton = this.FindControl<Button>(nameof(this.SignInButton));
-        var signUpButton = this.FindControl<Button>(nameof(this.CreateAccountButton));
+        var signInButton = this.FindControl<Label>(nameof(this.SignInButton));
+        var signUpButton = this.FindControl<TextBox>(nameof(this.CreateAccountButton));
 
-        if (logo == null || signInButton == null || signUpButton == null || signIn == null)
+        if (logo == null || signInButton == null || signUpButton == null )
             return;
 
         // Logo fade-in animation
@@ -49,15 +44,9 @@ public partial class MainViewMobile: UserControl
                                               {
                                                   await Task.Delay(1000);
                                                   logo.Opacity = 1;
+                                                  signInButton.Opacity = 1;
+                                                    signUpButton.Opacity = 1;
                                               });
 
-        // Buttons fade-in animation
-        await Dispatcher.UIThread.InvokeAsync(async () =>
-                                              {
-                                                  await Task.Delay(1000);
-                                                  signIn.Opacity       = 1;
-                                                  signInButton.Opacity = 1;
-                                                  signUpButton.Opacity = 1;
-                                              });
     }
 }
