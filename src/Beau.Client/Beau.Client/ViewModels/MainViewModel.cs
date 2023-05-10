9a8b67d1ec301d75b8f77f270182a44c87874c4e
davidchavezgrant <david@lines.by>
2023-05-10 01:53:20 -0400

    add reactive thing

--- src/Beau.Client/Beau.Client/ViewModels/MainViewModel.cs
+++ src/Beau.Client/Beau.Client/ViewModels/MainViewModel.cs
@@ -9,16 +9,18 @@ public class MainViewModel : ViewModelBase
 
     public string Greeting { get; private set; } = "What's your name?";
 
+    [Reactive]
     public string Input { get; set; } = "";
 
     [ObservableAsProperty]
-    public bool SendButtonIsVisible { get; set; } = true;
+    public bool SendButtonIsVisible { get; set; }
 
     public void SayHello() { Greeting = "Hello World!"; }
     public MainViewModel()
     {
         this.WhenAnyValue(x => x.Input)
-            .Select(x => !string.IsNullOrWhiteSpace(x) && x.Length > 1)
+            .Select(x => !string.IsNullOrWhiteSpace(x) && x.Length > 0)
+            .StartWith(false)
             .ToPropertyEx(this, x => x.SendButtonIsVisible);
     }
 }
