2511bf66d36fbce4cb9d291d7ace7941de46a358
davidchavezgrant <david@lines.by>
2023-05-10 01:49:43 -0400

    uh

--- src/Beau.Client/Beau.Client/ViewModels/MainViewModel.cs
+++ src/Beau.Client/Beau.Client/ViewModels/MainViewModel.cs
@@ -1,4 +1,8 @@
-﻿namespace Beau.Client.ViewModels;
+﻿using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+
+namespace Beau.Client.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
@@ -7,9 +11,14 @@ public class MainViewModel : ViewModelBase
 
     public string Input { get; set; } = "";
 
+    [ObservableAsProperty]
+    public bool SendButtonIsVisible { get; set; } = true;
+
     public void SayHello() { Greeting = "Hello World!"; }
     public MainViewModel()
     {
-
+        this.WhenAnyValue(x => x.Input)
+            .Select(x => !string.IsNullOrWhiteSpace(x) && x.Length > 1)
+            .ToPropertyEx(this, x => x.SendButtonIsVisible);
     }
 }
